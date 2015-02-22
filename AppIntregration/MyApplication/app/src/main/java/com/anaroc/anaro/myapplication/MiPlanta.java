ebad40747288e843b5bddd418d62139f7403ee9f
alvaronaro <alvaronaro28@gmail.com>
2015-02-22 17:10:15 +0100

    Comit MiPlanta V1
    Aun no comunica con el server.
    Para las graficas: http://www.android-graphview.org/

--- AppIntregration/MyApplication/app/src/main/java/com/anaroc/anaro/myapplication/MiPlanta.java
+++ AppIntregration/MyApplication/app/src/main/java/com/anaroc/anaro/myapplication/MiPlanta.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 /**
  * Created by Anaro on 10/02/2015.
@@ -15,6 +20,21 @@ public class MiPlanta extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.lay_miplanta, container, false);
+
+
+        GraphView graph = (GraphView) rootView.findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+
+        ImageView imagenplanta= (ImageView)rootView.findViewById(R.id.imageViewMiPlanta);
+        imagenplanta.setImageResource(R.drawable.imagen_planta_uno);
+
         return rootView;
     }
 }
