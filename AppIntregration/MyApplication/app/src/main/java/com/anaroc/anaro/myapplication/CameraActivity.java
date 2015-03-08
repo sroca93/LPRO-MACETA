d49e5ceb974ed4e8f5f00f71f0757a5d3fc51a24
luisrodrigueztrejo <luisrodriguezt@alumnos.uvigo.es>
2015-03-08 22:02:45 +0100

    Envío de imagen a servidor
    Devuelve código 200 pero no guarda la imagen

--- AppIntregration/MyApplication/app/src/main/java/com/anaroc/anaro/myapplication/CameraActivity.java
+++ AppIntregration/MyApplication/app/src/main/java/com/anaroc/anaro/myapplication/CameraActivity.java
@@ -1,9 +1,14 @@
 package com.anaroc.anaro.myapplication;
 
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -60,7 +65,7 @@ public class CameraActivity extends Activity {
             @Override
             public void onPictureTaken(byte[] data, Camera camera) {
 
-                File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+                final File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
                 if (pictureFile == null) {
                     Log.d(TAG, "Error creating media file, check storage permissions: ");
                     return;
@@ -95,6 +100,19 @@ public class CameraActivity extends Activity {
                     FileOutputStream fos = new FileOutputStream(pictureFile);
                     fos.write(data);
                     fos.close();
+                    new Thread(new Runnable() {
+                        public void run() {
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    // messageText.setText("uploading started.....");
+                                }
+                            });
+
+                            uploadFile(pictureFile.getAbsolutePath());
+
+                        }
+                    }).start();
+
 
                     onBackPressed();
 
@@ -106,7 +124,6 @@ public class CameraActivity extends Activity {
                 recreate();
             }
         };
-
         Button captureButton = (Button) findViewById(R.id.button_capture);
         captureButton.setOnClickListener(
                 new View.OnClickListener() {
@@ -117,6 +134,152 @@ public class CameraActivity extends Activity {
                     }
                 }
         );
+    }
+
+        public int uploadFile(String sourceFileUri) {
+
+
+            String fileName = sourceFileUri;
+
+            HttpURLConnection conn = null;
+            DataOutputStream dos = null;
+            String lineEnd = "\r\n";
+            String twoHyphens = "--";
+            String boundary = "*****";
+            int bytesRead, bytesAvailable, bufferSize;
+            byte[] buffer;
+            int maxBufferSize = 1 * 1024 * 1024;
+            File sourceFile = new File(sourceFileUri);
+
+            if (!sourceFile.isFile()) {
+
+                //dialog.dismiss();
+
+                Log.e("uploadFile", "Source File not exist :"
+                        + fileName);
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                   /* messageText.setText("Source File not exist :"
+                     -       +uploadFilePath + "" + uploadFileName);*/
+                    }
+                });
+
+                return 0;
+
+            } else {
+                int serverResponseCode = 0;
+                try {
+
+                    // open a URL connection to the Servlet
+                    FileInputStream fileInputStream = new FileInputStream(sourceFile);
+                    URL url = new URL("http://193.146.210.69/uploadImage.php");
+
+                    // Open a HTTP  connection to  the URL
+                    conn = (HttpURLConnection) url.openConnection();
+                    conn.setDoInput(true); // Allow Inputs
+                    conn.setDoOutput(true); // Allow Outputs
+                    conn.setUseCaches(false); // Don't use a Cached Copy
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Connection", "Keep-Alive");
+                    conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+                    conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                    conn.setRequestProperty("uploaded_file", fileName);
+
+                    dos = new DataOutputStream(conn.getOutputStream());
+
+                    dos.writeBytes(twoHyphens + boundary + lineEnd);
+                    dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\""
+                            + fileName + "" + lineEnd);
+
+                    dos.writeBytes(lineEnd);
+
+                    // create a buffer of  maximum size
+                    bytesAvailable = fileInputStream.available();
+
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    buffer = new byte[bufferSize];
+
+                    // read file and write it into form...
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                    while (bytesRead > 0) {
+
+                        dos.write(buffer, 0, bufferSize);
+                        bytesAvailable = fileInputStream.available();
+                        bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                        bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                    }
+
+                    // send multipart form data necesssary after file data...
+                    dos.writeBytes(lineEnd);
+                    dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                    // Responses from the server (code and message)
+                    serverResponseCode = conn.getResponseCode();
+                    String serverResponseMessage = conn.getResponseMessage();
+
+                    Log.i("uploadFile", "HTTP Response is : "
+                            + serverResponseMessage + ": " + serverResponseCode);
+
+                    if (serverResponseCode == 200) {
+
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+
+                            /*String msg = "File Upload Completed.\n\n See uploaded file here : \n\n"
+                                    +" http://www.androidexample.com/media/uploads/"
+                                    +uploadFileName;
+
+                            messageText.setText(msg);*/
+                                Toast.makeText(CameraActivity.this, "File Upload Complete.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+
+                    //close the streams //
+                    fileInputStream.close();
+                    dos.flush();
+                    dos.close();
+
+                } catch (MalformedURLException ex) {
+
+                    //dialog.dismiss();
+                    ex.printStackTrace();
+
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            //messageText.setText("MalformedURLException Exception : check script url.");
+                            Toast.makeText(CameraActivity.this, "MalformedURLException",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                    Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
+                } catch (Exception e) {
+
+                    // dialog.dismiss();
+                    e.printStackTrace();
+
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            //messageText.setText("Got Exception : see logcat ");
+                            Toast.makeText(CameraActivity.this, "Got Exception : see logcat ",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    Log.e("Upload file Exception", "Exception : "
+                            + e.getMessage(), e);
+                }
+                //dialog.dismiss();
+                return serverResponseCode;
+
+            } // End else block
+
+
+
     }
 
 
