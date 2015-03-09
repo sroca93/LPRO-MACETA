a00376986163c2c8d608e5330c1dd439242602ff
luisrodrigueztrejo <luisrodriguezt@alumnos.uvigo.es>
2015-03-09 23:00:13 +0100

    Envío de fotos al servidor

--- AppIntregration/MyApplication/app/src/main/java/com/anaroc/anaro/myapplication/CameraActivity.java
+++ AppIntregration/MyApplication/app/src/main/java/com/anaroc/anaro/myapplication/CameraActivity.java
@@ -1,26 +1,24 @@
 package com.anaroc.anaro.myapplication;
 
-import java.io.DataOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -28,6 +26,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
 import contenedores.Parametro;
 
@@ -38,7 +40,11 @@ public class CameraActivity extends Activity {
     private Camera mCamera;
     private CameraPreview mPreview;
     private static Context context;
-
+    String encodedString;
+    RequestParams params = new RequestParams();
+    String imgPath, fileName;
+    Bitmap bitmap;
+    private static int RESULT_LOAD_IMG = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -107,8 +113,14 @@ public class CameraActivity extends Activity {
                                     // messageText.setText("uploading started.....");
                                 }
                             });
+                            imgPath = pictureFile.getAbsolutePath();
+                            fileName = pictureFile.getName();
+                            Log.d("imgPath", imgPath);
+                            Log.d("fileName", fileName);
 
-                            uploadFile(pictureFile.getAbsolutePath());
+                            params.put("filename", fileName);
+                            uploadImage();
+                            //uploadFile(pictureFile.getAbsolutePath());
 
                         }
                     }).start();
@@ -136,152 +148,101 @@ public class CameraActivity extends Activity {
         );
     }
 
-        public int uploadFile(String sourceFileUri) {
+    public void uploadImage() {
+        // When Image is selected from Gallery
 
-
-            String fileName = sourceFileUri;
-
-            HttpURLConnection conn = null;
-            DataOutputStream dos = null;
-            String lineEnd = "\r\n";
-            String twoHyphens = "--";
-            String boundary = "*****";
-            int bytesRead, bytesAvailable, bufferSize;
-            byte[] buffer;
-            int maxBufferSize = 1 * 1024 * 1024;
-            File sourceFile = new File(sourceFileUri);
-
-            if (!sourceFile.isFile()) {
-
-                //dialog.dismiss();
-
-                Log.e("uploadFile", "Source File not exist :"
-                        + fileName);
-
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                   /* messageText.setText("Source File not exist :"
-                     -       +uploadFilePath + "" + uploadFileName);*/
-                    }
-                });
-
-                return 0;
-
-            } else {
-                int serverResponseCode = 0;
-                try {
-
-                    // open a URL connection to the Servlet
-                    FileInputStream fileInputStream = new FileInputStream(sourceFile);
-                    URL url = new URL("http://193.146.210.69/uploadImage.php");
-
-                    // Open a HTTP  connection to  the URL
-                    conn = (HttpURLConnection) url.openConnection();
-                    conn.setDoInput(true); // Allow Inputs
-                    conn.setDoOutput(true); // Allow Outputs
-                    conn.setUseCaches(false); // Don't use a Cached Copy
-                    conn.setRequestMethod("POST");
-                    conn.setRequestProperty("Connection", "Keep-Alive");
-                    conn.setRequestProperty("ENCTYPE", "multipart/form-data");
-                    conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
-                    conn.setRequestProperty("uploaded_file", fileName);
-
-                    dos = new DataOutputStream(conn.getOutputStream());
-
-                    dos.writeBytes(twoHyphens + boundary + lineEnd);
-                    dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\""
-                            + fileName + "" + lineEnd);
-
-                    dos.writeBytes(lineEnd);
-
-                    // create a buffer of  maximum size
-                    bytesAvailable = fileInputStream.available();
-
-                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                    buffer = new byte[bufferSize];
-
-                    // read file and write it into form...
-                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-
-                    while (bytesRead > 0) {
-
-                        dos.write(buffer, 0, bufferSize);
-                        bytesAvailable = fileInputStream.available();
-                        bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                        bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-
-                    }
-
-                    // send multipart form data necesssary after file data...
-                    dos.writeBytes(lineEnd);
-                    dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
-
-                    // Responses from the server (code and message)
-                    serverResponseCode = conn.getResponseCode();
-                    String serverResponseMessage = conn.getResponseMessage();
-
-                    Log.i("uploadFile", "HTTP Response is : "
-                            + serverResponseMessage + ": " + serverResponseCode);
-
-                    if (serverResponseCode == 200) {
-
-                        runOnUiThread(new Runnable() {
-                            public void run() {
-
-                            /*String msg = "File Upload Completed.\n\n See uploaded file here : \n\n"
-                                    +" http://www.androidexample.com/media/uploads/"
-                                    +uploadFileName;
-
-                            messageText.setText(msg);*/
-                                Toast.makeText(CameraActivity.this, "File Upload Complete.",
-                                        Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    }
-
-                    //close the streams //
-                    fileInputStream.close();
-                    dos.flush();
-                    dos.close();
-
-                } catch (MalformedURLException ex) {
-
-                    //dialog.dismiss();
-                    ex.printStackTrace();
-
-                    runOnUiThread(new Runnable() {
-                        public void run() {
-                            //messageText.setText("MalformedURLException Exception : check script url.");
-                            Toast.makeText(CameraActivity.this, "MalformedURLException",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    });
-
-                    Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
-                } catch (Exception e) {
-
-                    // dialog.dismiss();
-                    e.printStackTrace();
-
-                    runOnUiThread(new Runnable() {
-                        public void run() {
-                            //messageText.setText("Got Exception : see logcat ");
-                            Toast.makeText(CameraActivity.this, "Got Exception : see logcat ",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                    Log.e("Upload file Exception", "Exception : "
-                            + e.getMessage(), e);
-                }
-                //dialog.dismiss();
-                return serverResponseCode;
-
-            } // End else block
-
-
+            encodeImagetoString();
 
     }
 
+    public void encodeImagetoString() {
+        new AsyncTask<Void, Void, String>() {
+
+            protected void onPreExecute() {
+
+            };
+
+            @Override
+            protected String doInBackground(Void... params) {
+                BitmapFactory.Options options;
+                options = new BitmapFactory.Options();
+                options.inSampleSize = 3;
+                bitmap = BitmapFactory.decodeFile(imgPath,
+                        options);
+                if (bitmap == null) Log.d("bitmap", "null");
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                // Must compress the Image to reduce image size to make upload easy
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 50, stream);
+                byte[] byte_arr = stream.toByteArray();
+                // Encode Image to String
+                encodedString = Base64.encodeToString(byte_arr, 0);
+                return "";
+            }
+
+            @Override
+            protected void onPostExecute(String msg) {
+                //prgDialog.setMessage("Calling Upload");
+                // Put converted Image string into Async Http Post param
+                params.put("image", encodedString);
+                // Trigger Image upload
+                triggerImageUpload();
+            }
+        }.execute(null, null, null);
+    }
+
+    public void triggerImageUpload() {
+        makeHTTPCall();
+    }
+
+    // Make Http call to upload Image to Php server
+    public void makeHTTPCall() {
+        //prgDialog.setMessage("Invoking Php");
+        AsyncHttpClient client = new AsyncHttpClient();
+        // Don't forget to change the IP address to your LAN address. Port no as well.
+
+        client.post("http://193.146.210.69/uploadImage.php",
+                params, new AsyncHttpResponseHandler() {
+                    // When the response returned by REST has Http
+                    // response code '200'
+                    @Override
+                    public void onSuccess(String response) {
+                        // Hide Progress Dialog
+                        //prgDialog.hide();
+                        Toast.makeText(getApplicationContext(), response,
+                                Toast.LENGTH_LONG).show();
+                    }
+
+                    // When the response returned by REST has Http
+                    // response code other than '200' such as '404',
+                    // '500' or '403' etc
+                    @Override
+                    public void onFailure(int statusCode, Throwable error,
+                                          String content) {
+                        // Hide Progress Dialog
+                        //prgDialog.hide();
+                        // When Http response code is '404'
+                        if (statusCode == 404) {
+                            Toast.makeText(getApplicationContext(),
+                                    "Requested resource not found",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                        // When Http response code is '500'
+                        else if (statusCode == 500) {
+                            Toast.makeText(getApplicationContext(),
+                                    "Something went wrong at server end",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                        // When Http response code other than 404, 500
+                        else {
+                            Toast.makeText(
+                                    getApplicationContext(),
+                                    "Error Occured \n Most Common Error: \n1. Device not connected to Internet\n2. Web App is not deployed in App server\n3. App server is not running\n HTTP Status code : "
+                                            + statusCode, Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    }
+                });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -371,14 +332,6 @@ public class CameraActivity extends Activity {
         }
     }
 
-    public class SendPhotoTask extends AsyncTask<Parametro, Void, Boolean> {
 
-
-        @Override
-        protected Boolean doInBackground(Parametro... params) {
-            double respuestaJSON = Double.parseDouble(Consultas.hacerConsulta(params));
-            return respuestaJSON == 0;
-        }
-    }
 
 }
