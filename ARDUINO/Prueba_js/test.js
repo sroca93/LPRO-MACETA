b7dfe2e5a3884fc48bf9c6c64fed7c91a49b882f
sroca93 <simonaaf@gmail.com>
2015-03-14 12:46:33 +0100

    javascript funciona bien, sube medidas y las consulta(puede comentarse para que no compruebe cada dos por tres)

--- ARDUINO/Prueba_js/test.js
+++ ARDUINO/Prueba_js/test.js
@@ -1,11 +1,15 @@
 //DEPENDENCIES
 fs = require('fs');
 moment = require('moment');
-var n = 3;
+
 //CONFIG PARAMETERS
+
+var n = 3;
+var interv = 1; //tiempo entre medidas, en minutos 
 var minutes = 0.1; //minutes between every check
 var path_file = "./capture.txt"; //watched file
 var verbose = true; //do you want dates and event printed?
+
 
 //AUX FUNCTIONS
 
@@ -34,18 +38,19 @@ var callback = function(){
 	}else{
 		Comment("Nothing has changed.");
 		Comment("LastMeasure: ");
-		//ultimaMedicion();
+		ultimaMedicion();
 	}
 };
 
 
 ////PROGRAM
 
-console.log("\n\n\n");
+Comment("\n\n\n");
 var fecham = fs.statSync(path_file).mtime;
-Comment('Original last modification date: '+moment(fecham).format('MMMM Do YYYY, h:mm:ss a'));
+//Comment('Original last modification date: '+moment(fecham).format('MMMM Do YYYY, h:mm:ss a'));
 
 setInterval(callback, minutes*60*1000);
+
 
 //CONSULTA UTILS
 var http = require("http"); //para hacer peticiones http
@@ -62,13 +67,49 @@ eventEmitter.removeListener(current_time, imprimeBody);
 
 };
 
+//CONSULTA GENÉRICA
+function consultaPHP(consulta,parametros){
+	var current_time = Date.now();
+	eventEmitter.on(current_time, imprimeBody);
+	
+	var options = {
+  		host: '193.146.210.69',
+  		port: 80,
+  		path: '/consultas.php?consulta='+consulta,
+  		method: 'GET'
+		};
+		
+	
+	for (key in parametros) {
+    		if (parametros.hasOwnProperty(key)) {
+        		options.path = options.path + "&"+key+"="+parametros[key];
+    		}
+	}  
+	Comment("Iniciando consulta: "+ options.path);
+	var req = http.request(options, function(res) {
+  		Comment('RESPUESTA: ' + res.statusCode);
+  		//Comment('HEADERS: ' + JSON.stringify(res.headers));
+  		res.setEncoding('utf8');
+  		res.on('data', function (chunk) {
+    			//Comment('BODY: ' + chunk);
+			eventEmitter.emit(current_time, current_time, chunk);
+  		});
+		
+	});
+
+	req.on('error', function(e) {
+  		Comment('problem with request: ' + e.message);
+	});
+	req.end();
+}
+
+
 //GUARDARMEDICION
 function guardarMedicion(){
 
 	fs.readFile(path_file, 'utf8', function(err, data) {
 			  if (err) throw err;
-			  Comment('OK: ' + path_file);
-			  //Comment(data);
+			  
 var pieces = data.split("\n");
 
 var send="";
@@ -76,12 +117,23 @@ for(i=0;i<=n;i++){
 send+=pieces.pop()+'*';
 }
 
-send = send.substring(0, send.length-1);
+send = send.substring(1, send.length-1);
 send= send.replace('\n','');
 
-Comment(send);
+Comment("DATOS A ENVIAR: "+send);
+var parametros = {
+			datos: send,
+			intervalo: interv
+		};
+		Comment(parametros.toString());
+		var miConsulta = "guardarMedicion"; 
+		consultaPHP(miConsulta,parametros);
+
+
 
 		});
+
+
 		
 
 };
@@ -91,10 +143,11 @@ Comment(send);
 function ultimaMedicion(){
 	var parametros = {
 			plantID: 3,
-			numero: 1
+			numero: n
 		};
 
 		var miConsulta = "obtenerMediciones"; 
+		Comment("Últimas "+ n+ " medidas:\n");
 		consultaPHP(miConsulta,parametros);	
 
 };
