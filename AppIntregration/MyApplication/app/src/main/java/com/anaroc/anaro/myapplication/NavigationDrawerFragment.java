a74e1f967f2adeaa7e5b793dd5d5f2b67a4b4b8c
luisrodrigueztrejo <luisrodriguezt@alumnos.uvigo.es>
2015-03-25 00:00:33 +0100

    Solucionado error menor

--- AppIntregration/MyApplication/app/src/main/java/com/anaroc/anaro/myapplication/NavigationDrawerFragment.java
+++ AppIntregration/MyApplication/app/src/main/java/com/anaroc/anaro/myapplication/NavigationDrawerFragment.java
@@ -129,7 +129,9 @@ public class NavigationDrawerFragment extends Fragment {
         TextView textViewDrawer = (TextView)  drawerView.findViewById(R.id.textViewDrawer);
 
         String nombreUsuario = PrefUtils.getFromPrefs(this.getActivity(), "ACTUAL_USERNAME","");
-        textViewDrawer.setText(Character.toUpperCase(nombreUsuario.charAt(0)) + nombreUsuario.substring(1));
+        Log.d("nombreUsuario", nombreUsuario);
+        //textViewDrawer.setText(Character.toUpperCase(nombreUsuario.charAt(0)) + nombreUsuario.substring(1));
+        textViewDrawer.setText(nombreUsuario.substring(0,1).toUpperCase() + nombreUsuario.substring(1));
 
 
         return drawerView;
